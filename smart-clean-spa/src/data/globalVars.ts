7d32afab6ff64332da591989a2f3d58917c8699b
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-07-29 23:20:35 +0800

    added in the endpoint list

---
+++ smart-clean-spa/src/data/globalVars.ts
@@ -0,0 +1,46 @@
+export const GlobalVars = {
+    apiUri : ' https://os034igan7.execute-api.ap-southeast-1.amazonaws.com/production/',
+    auth: {
+        login: 'login',
+        register: 'register',
+    },
+    project: {
+        get: {
+            projectList: 'projects',
+            completeProject: 'project',
+        },
+        post : {
+            createProject: 'project'
+        },
+        put: {
+            updateProject: 'project'
+        }
+
+    },
+    location: {
+        get: {
+            locationListByUser: 'location/user',
+            /** require projectId to be added to find the location */
+            locationListByProject: 'location/project/',
+        },
+        post : {
+            createLocation: 'location'
+        },
+        put: {
+            updateLocation: 'location'
+        }
+    },
+    device: {
+        get: {
+            deviceListByUser: 'device/user',
+            /** Require locationId to find the devices at that location */
+            deviceListByLocation: 'device/location/',
+        },
+        post : {
+            createDevice: 'device'
+        },
+        put: {
+            updateDevice: 'device'
+        }
+    }
+};
