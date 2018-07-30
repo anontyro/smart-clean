152cc1e1a23aabff279a1bb6b7444e5519c56311
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-07-30 23:03:58 +0800

    Basic login has ben added this will push the data to the API and then check the values are correct displays user feedback in case of errors and has basic form validation probaly need to improve for email pattern as well

--- smart-clean-spa/src/app/core/services/auth/login-handler.service.ts
+++ smart-clean-spa/src/app/core/services/auth/login-handler.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Observable } from '../../../../../node_modules/rxjs';
 import { HttpClient } from '@angular/common/http';
 import { GlobalVars } from '../../../../data/globalVars';
 import { LoginModel } from '../../../../models/auth/login.model';
 import { TokenModel } from '../../../../models/auth/token.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -14,15 +15,39 @@ export class LoginHandlerService {
   private loginUri = GlobalVars.apiUri + GlobalVars.auth.login;
   private tokenStorage = GlobalVars.tokenStorage;
 
-  constructor(private http: HttpClient) { }
+  public loginErrorEmitter: EventEmitter<any> = new EventEmitter();
 
-  public getNewToken(user: LoginModel) {
+  public ErrorMessage = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  public getNewToken(user: LoginModel, returnUrl: string) {
+
     const item = { email: user.username, password: user.password};
 
     this.createPostRequest(this.loginUri, item).subscribe((token: TokenModel) => {
-      console.log(token);
-    });
+      if (!token.token || token.token.length < 5) {
+        return this.loginError('Unable to obtain the token from server');
+      }
 
+      this.loginSuccessful(token, returnUrl);
+    }, err => this.loginError('Unable to contact server'));
+
+  }
+
+  public logout() {
+    localStorage.removeItem(this.tokenStorage);
+  }
+
+  private loginSuccessful(token: TokenModel, returnUrl: string) {
+    localStorage.setItem(this.tokenStorage, JSON.stringify(token));
+    this.router.navigate([returnUrl]);
+  }
+
+  public loginError(message) {
+    this.loginErrorEmitter.emit(message);
+    this.ErrorMessage = message;
+    console.error(message);
   }
 
     /**
--- smart-clean-spa/src/app/features/auth-views/login-view/login-view.component.ts
+++ smart-clean-spa/src/app/features/auth-views/login-view/login-view.component.ts
@@ -1,33 +1,60 @@
 import { LoginHandlerService } from './../../../core/services/auth/login-handler.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { LoginModel } from '../../../../models/auth/login.model';
 import { NgForm } from '../../../../../node_modules/@angular/forms';
 import { AuthService } from '../../../core/services/auth/auth.service';
+import { ActivatedRoute } from '../../../../../node_modules/@angular/router';
+import {takeWhile} from 'rxjs/operators';
 
 @Component({
   selector: 'app-login-view',
   templateUrl: './login-view.component.html',
   styleUrls: ['./login-view.component.scss']
 })
-export class LoginViewComponent implements OnInit {
+export class LoginViewComponent implements OnInit, OnDestroy {
+
 
   public userLogin: LoginModel = new LoginModel('', '', true);
-
+  public loginError = '';
   public isLoading = false;
 
-  constructor(private loginService: LoginHandlerService) { }
+  private keepAlive = true;
+
+  private returnUrl;
+
+  constructor(
+    private loginService: LoginHandlerService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.loginService.logout();
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    this.listeners();
+  }
+
+  private listeners() {
+    this.loginService.loginErrorEmitter
+      .pipe(takeWhile(() => this.keepAlive))
+      .subscribe(response => {
+        this.isLoading = false;
+        this.loginError = response;
+    });
   }
 
   public onSubmit(loginForm: NgForm) {
+    this.loginError = '';
     if (loginForm.invalid) {
       this.loginService.setFormFieldErrors(loginForm);
+      this.loginError = 'Ensure all form fields are entered correctly';
       return;
     }
-    this.loginService.getNewToken(this.userLogin);
-    return console.log('valid');
+    this.isLoading = true;
+    this.loginService.getNewToken(this.userLogin, this.returnUrl);
 
   }
 
+  ngOnDestroy(): void {
+    this.keepAlive = false;
+  }
 }
