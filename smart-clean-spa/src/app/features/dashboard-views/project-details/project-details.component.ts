6ac24a195a83fdce7b5b1a34b70165014eeef47b
Alexander Wilkinson <alexwilkinson@gmail.com>
2018-07-31 23:45:32 +0800

    starting on the detail object data is being sent just need to organise in hte best way and allow for attaching devices and locations

--- smart-clean-spa/src/app/features/dashboard-views/project-details/project-details.component.ts
+++ smart-clean-spa/src/app/features/dashboard-views/project-details/project-details.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { ApiHandlerService } from './../../../core/services/api/api-handler.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '../../../../../node_modules/@angular/router';
+import { takeWhile } from '../../../../../node_modules/rxjs/operators';
+import { ProjectCompleteModel } from '../../../../models/database/project.model';
 
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
   styleUrls: ['./project-details.component.scss']
 })
-export class ProjectDetailsComponent implements OnInit {
+export class ProjectDetailsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public isLoading = false;
+  public project: ProjectCompleteModel;
+
+  private projectId: string;
+  private keepAlive = true;
+
+  constructor(
+    private route: ActivatedRoute,
+    private apiHandlerService: ApiHandlerService
+  ) { }
 
   ngOnInit() {
+    this.route.params
+      .pipe(takeWhile(() => this.keepAlive))
+      .subscribe(params => {
+        this.projectId = params['id'];
+        this.buildPage();
+      });
+  }
+
+  private buildPage() {
+    this.isLoading = true;
+    this.apiHandlerService.getProjectDetail(this.projectId)
+      .pipe(takeWhile(() => this.keepAlive))
+      .subscribe(response => {
+        this.project = response.project;
+        this.isLoading = false;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.keepAlive = false;
   }
 
 }
